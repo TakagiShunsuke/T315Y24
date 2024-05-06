024724314a2aa7880863dd34dc50e12c27a9ff69
YamamotoKaito <pepe@WNHS20037>
2024-05-06 15:28:40 +0900

    コメント修正

--- T315Y24/Assets/Script/CMine.cs
+++ T315Y24/Assets/Script/CMine.cs
@@ -1,37 +1,38 @@
 /*=====
-<Mine.cs> 
+<CMine.cs> 
 └作成者：yamamoto
 
 ＞内容
 地雷オブジェクトにアタッチするスクリプト
 
-＞注意事項   //ないときは省略OK
-この規約書に記述のないものは判明次第、適宜追加する
-
 ＞更新履歴
-__Y24   //'24年
-_M05    //05月
-D       //日
+__Y24
+_M05
+D
 03:プログラム作成:yamamoto
 06:インターフェース追加:yamamoto
 
 =====*/
+
+//＞名前空間宣言
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//＞クラス定義
 public class CMine : MonoBehaviour
 {
-    public float ExplosionRadius = 5f;  // 地雷爆発範囲
-    public float ExplosionDelay = 2f;   // 爆発までの待機時間
-    public float RechargeDelay = 7f;    // 再充電までの待機時間
-    public LayerMask EnemyLayer;        // 地雷の影響を受けるオブジェクトを指定
+    //＞変数宣言
+    [SerializeField] private double ExplosionRadius = 5.0d;  // 地雷爆発範囲
+    [SerializeField] private double ExplosionDelay = 2.0d;   // 爆発までの待機時間
+    [SerializeField] private double RechargeDelay = 7.0d;    // 再利用までの時間
+    private LayerMask EnemyLayer;        // 地雷の影響を受けるオブジェクトを指定
+    private bool CanExplode = true;     //地雷利用 true:可能 false:不可
 
-    private bool CanExplode = true;
-
+    //＞インターフェース定義
     public interface IDestroy
     { 
-        void TakeDestroy();
+        void TakeDestroy();     //オブジェクトの破壊
     }
 
     /*＞地雷当たり判定関数
@@ -39,9 +40,9 @@ public class CMine : MonoBehaviour
     ｘ
     戻値：なし
     ｘ
-    概要：敵が地雷に触れたときのみ爆発する
+    概要：敵が地雷に触れたときのみ処理される
     */
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other)     //地雷に何かが当たってきたとき
     {
         if (other.CompareTag("Enemy")&&CanExplode)  //Enemyタグがついている＆地雷使用可能
         {
@@ -50,7 +51,7 @@ public class CMine : MonoBehaviour
         }
     }
 
-    /*＞地雷再利用の判定の関数
+    /*＞地雷再利用時間計測の関数
     引数１：なし
     ｘ
     戻値：なし
@@ -59,9 +60,9 @@ public class CMine : MonoBehaviour
     */
     IEnumerator RechargeMine()
     {
-        CanExplode = false;
-        yield return new WaitForSeconds(RechargeDelay);
-        CanExplode = true;
+        CanExplode = false;     //地雷使用不可
+        yield return new WaitForSeconds((float)RechargeDelay);      //引数分待機する
+        CanExplode = true;      //待機が終わると地雷使用可能にする
     }
 
     /*＞爆発時差関数
@@ -73,7 +74,7 @@ public class CMine : MonoBehaviour
    */
     IEnumerator ExplodeAfterDelay()
     {
-        yield return new WaitForSeconds(ExplosionDelay);
+        yield return new WaitForSeconds((float)ExplosionDelay);     //引数分待機する
 
         // 爆発処理をここに記述
         Explode();
@@ -88,16 +89,16 @@ public class CMine : MonoBehaviour
     */
     void Explode()
     {
-        //地雷の範囲に入ってる敵のみ配列に入れる
-        Collider[] colliders = Physics.OverlapSphere(transform.position, ExplosionRadius, EnemyLayer);
+        //地雷の範囲に入ってる敵（EnemyLayer）のみ配列に入れる
+        Collider[] colliders = Physics.OverlapSphere(transform.position, (float)ExplosionRadius, EnemyLayer);
 
         foreach (Collider hit in colliders) //collidersの中に入ってる全部for文で繰り返す
         {
             // IDestroyを実装していたら
             if (hit.gameObject.TryGetComponent<IDestroy>(out var destroy))
-                if (destroy != null)
+                if (destroy != null)            //取得成功しているか
                 {
-                    destroy.TakeDestroy();
+                    destroy.TakeDestroy();      //破壊の処理をする
                 } 
         }
     }
