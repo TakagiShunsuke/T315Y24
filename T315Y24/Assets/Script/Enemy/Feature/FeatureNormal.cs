95717aadc64cb97c39d2426a0ec0884c0de7b99a
TakagiShunsuke <149446567+TakagiShunsuke@users.noreply.github.com>
2024-05-13 11:04:04 +0900

    Revert "Merge branch 'develop' into Featuredestroy"
    This reverts commit 01b482183bcda8c6a741c92b1bb3ccc15388da4b, reversing
    changes made to 96622d52803b228582c7d27a06cbae3b75270970.

--- T315Y24/Assets/Script/Enemy/Feature/FeatureNormal.cs
+++ T315Y24/Assets/Script/Enemy/Feature/FeatureNormal.cs
@@ -11,7 +11,6 @@ _M05
 D
 03:プログラム作成:takagi
 04:コメント修正:takagi
-11:速度修正:takagi
 =====*/
 
 //＞名前空間宣言
@@ -21,7 +20,7 @@ using UnityEngine;  //Unity
 public class CFeatureNormal : MonoBehaviour, IFeature
 {
     //＞プロパティ定義
-    public double Atk { get; } = 1.0d;   //攻撃力
+    public double Atk { get; } = 40.0d;   //攻撃力
     public double Move { get; } = 4.0d;  //移動距離[m/s]
     public string Information { get; } = "もに基準";  //詳細情報
 }
